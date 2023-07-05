50f4e79d8712eb9eba6c0169432a196835347ba8
Merseyside <ivanklessablin@gmail.com>
2023-07-05 23:55:01 +0600

    Comment some stuff to reproduce issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,17 +10,17 @@ tasks.register("clean", Delete::class).configure {
     delete(rootProject.buildDir)
 }
 
-val javadocDisabledModules = listOf(
-    "adapters-delegates"
-)
-
-subprojects {
-    gradle.taskGraph.whenReady {
-        if (javadocDisabledModules.contains(this@subprojects.name)) {
-            tasks.matching { it.name == "javaDocReleaseGeneration" }.configureEach {
-                // See: https://youtrack.jetbrains.com/issue/KTIJ-19005/JDK-17-PermittedSubclasses-requires-ASM9-exception-multiple-times-per-second-during-analysis
-                enabled = false
-            }
-        }
-    }
-}
+//val javadocDisabledModules = listOf(
+//    "adapters-delegates"
+//)
+//
+//subprojects {
+//    gradle.taskGraph.whenReady {
+//        if (javadocDisabledModules.contains(this@subprojects.name)) {
+//            tasks.matching { it.name == "javaDocReleaseGeneration" }.configureEach {
+//                // See: https://youtrack.jetbrains.com/issue/KTIJ-19005/JDK-17-PermittedSubclasses-requires-ASM9-exception-multiple-times-per-second-during-analysis
+//                enabled = false
+//            }
+//        }
+//    }
+//}
